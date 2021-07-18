a4cf4280dcfdfd8541603975ad5516a695c408f3
arkya <arkyabagchi@gmail.com>
2021-07-18 14:47:49 +0530

    Initial Commit

---
+++ Absentism-Database.sql
@@ -0,0 +1,7 @@
+DROP DATABASE IF EXISTS predicted_outputs;
+
+CREATE DATABASE IF NOT EXISTS predicted_outputs;
+USE predicted_outputs;
+
+DROP TABLE IF EXISTS predicted_outputs;
+CREATE TABLE predicted_outputs
