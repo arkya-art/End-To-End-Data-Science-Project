def9a41424c075924b527d16d9acb2a2ebbf9f50
arkya <arkyabagchi@gmail.com>
2021-07-18 15:23:25 +0530

    Model snapshot & Tableau files modified

--- Absentism-Database.sql
+++ Absentism-Database.sql
@@ -5,3 +5,20 @@ USE predicted_outputs;
 
 DROP TABLE IF EXISTS predicted_outputs;
 CREATE TABLE predicted_outputs
+(   reason_1 BIT NOT NULL,
+	reason_2 BIT NOT NULL,
+    reason_3 BIT NOT NULL,
+    reason_4 BIT NOT NULL,
+    month_value INT NOT NULL,
+    transportation_expense INT NOT NULL,
+    age INT NOT NULL,
+    body_mass_index INT NOT NULL,
+    education BIT NOT NULL,
+    children INT NOT NULL,
+    pets INT NOT NULL,
+    probability FLOAT NOT NULL,
+    prediction BIT NOT NULL
+);
+
+SELECT * FROM predicted_outputs
+
